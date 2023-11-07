712c82a33d537e8fddeeba59c0610a7ce4d1dd26
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2023-11-07 16:59:07 -0300

    Correct BrLite and Peripherals connection

--- rtl/PhiversMC.sv
+++ rtl/PhiversMC.sv
@@ -49,6 +49,25 @@ module PhiversMC
 
     localparam n_pe = N_PE_X * N_PE_Y;
 
+    /* Hermes signals */
+    logic                           release_peripheral [(N_PE_X - 1):0][(N_PE_Y - 1):0];
+    logic                           rx                 [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
+    logic                           credit_rx          [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
+    logic        [31:0]             data_rx            [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
+
+    logic                           tx                 [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
+    logic                           credit_tx          [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
+    logic        [31:0]             data_tx            [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
+
+    /* BrLite signals */
+    logic        [(BR_NPORT - 2):0] req_rx             [(N_PE_X - 1):0][(N_PE_Y - 1):0];
+    logic        [(BR_NPORT - 2):0] ack_rx             [(N_PE_X - 1):0][(N_PE_Y - 1):0];
+    br_data_t    [(BR_NPORT - 2):0] flit_rx            [(N_PE_X - 1):0][(N_PE_Y - 1):0];
+
+    logic        [(BR_NPORT - 2):0] req_tx             [(N_PE_X - 1):0][(N_PE_Y - 1):0];
+    logic        [(BR_NPORT - 2):0] ack_tx             [(N_PE_X - 1):0][(N_PE_Y - 1):0];
+    br_data_t    [(BR_NPORT - 2):0] flit_tx            [(N_PE_X - 1):0][(N_PE_Y - 1):0];
+
 ////////////////////////////////////////////////////////////////////////////////
 // MA Injector
 ////////////////////////////////////////////////////////////////////////////////
@@ -82,6 +101,14 @@ module PhiversMC
         .noc_data_i      (ma_inj_data_rx  )
     );
 
+    always_comb begin
+        localparam logic [7:0] x = ADDR_MA_INJ[15:8];
+        localparam logic [7:0] y = ADDR_MA_INJ[7:0];
+        ma_inj_rx        = tx[x][y][PORT_MA_INJ];
+        ma_inj_credit_tx = credit_rx[x][y][PORT_MA_INJ];
+        ma_inj_data_rx   = data_tx[x][y][PORT_MA_INJ];
+    end
+
 ////////////////////////////////////////////////////////////////////////////////
 // App Injector
 ////////////////////////////////////////////////////////////////////////////////
@@ -95,10 +122,10 @@ module PhiversMC
     logic [31:0] app_inj_data_rx;
 
     TaskInjector #(
-        .INJECTOR_ADDRESS(ADDR_APP_INJ),
-        .FLIT_SIZE(32),
-        .MAX_PAYLOAD_SIZE(32),
-        .INJECT_MAPPER(0)
+        .INJECTOR_ADDRESS (ADDR_APP_INJ),
+        .FLIT_SIZE        (32          ),
+        .MAX_PAYLOAD_SIZE (32          ),
+        .INJECT_MAPPER    (0           )
     )
     AppInjector (
         .clk_i           (clk_i            ),
@@ -115,33 +142,22 @@ module PhiversMC
         .noc_data_i      (app_inj_data_rx  )
     );
 
+    always_comb begin
+        localparam logic [7:0] x = ADDR_APP_INJ[15:8];
+        localparam logic [7:0] y = ADDR_APP_INJ[7:0];
+        app_inj_rx        = tx[x][y][PORT_APP_INJ] && release_peripheral[x][y];
+        app_inj_credit_tx = credit_rx[x][y][PORT_APP_INJ];
+        app_inj_data_rx   = data_tx[x][y][PORT_APP_INJ];
+    end
+
 ////////////////////////////////////////////////////////////////////////////////
 // Add new peripherals here
 ////////////////////////////////////////////////////////////////////////////////
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// Many-core Injector
+// Many-core generation
 ////////////////////////////////////////////////////////////////////////////////
-
-    /* Hermes signals */
-    logic        release_peripheral [(N_PE_X - 1):0][(N_PE_Y - 1):0];
-    logic        rx                 [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
-    logic        credit_rx          [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
-    logic [31:0] data_rx            [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
-
-    logic        tx                 [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
-    logic        credit_tx          [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
-    logic [31:0] data_tx            [(N_PE_X - 1):0][(N_PE_Y - 1):0][(HERMES_NPORT - 2):0];
-
-    /* BrLite signals */
-    logic        req_rx             [(N_PE_X - 1):0][(N_PE_Y - 1):0][(BR_NPORT - 2):0];
-    logic        ack_rx             [(N_PE_X - 1):0][(N_PE_Y - 1):0][(BR_NPORT - 2):0];
-    br_data_t    flit_rx            [(N_PE_X - 1):0][(N_PE_Y - 1):0][(BR_NPORT - 2):0];
-
-    logic        req_tx             [(N_PE_X - 1):0][(N_PE_Y - 1):0][(BR_NPORT - 2):0];
-    logic        ack_tx             [(N_PE_X - 1):0][(N_PE_Y - 1):0][(BR_NPORT - 2):0];
-    br_data_t    flit_tx            [(N_PE_X - 1):0][(N_PE_Y - 1):0][(BR_NPORT - 2):0];
 
     generate
         for (genvar x = 0; x < N_PE_X; x++) begin : gen_x
@@ -150,11 +166,11 @@ module PhiversMC
                 localparam logic [15:0] seq_addr = y * N_PE_X + x;
 
                 PhiversPE #(
-                    .ADDRESS(address),
-                    .SEQ_ADDRESS(seq_addr),
-                    .N_PE(n_pe),
-                    .TASKS_PER_PE(TASKS_PER_PE),
-                    .Environment(Environment)
+                    .ADDRESS      (address     ),
+                    .SEQ_ADDRESS  (seq_addr    ),
+                    .N_PE         (n_pe        ),
+                    .TASKS_PER_PE (TASKS_PER_PE),
+                    .Environment  (Environment )
                 ) 
                 pe (
                     .clk_i                (clk_i                   ),
@@ -177,7 +193,7 @@ module PhiversMC
                     .noc_rx_i             (rx[x][y]                ),
                     .noc_credit_o         (credit_rx[x][y]         ),
                     .noc_data_i           (data_rx[x][y]           ),
-                    .noc_tx_i             (tx[x][y]                ),
+                    .noc_tx_o             (tx[x][y]                ),
                     .noc_credit_i         (credit_tx[x][y]         ),
                     .noc_data_o           (data_tx[x][y]           ),
                     .brlite_req_i         (req_rx[x][y]            ),
@@ -187,66 +203,78 @@ module PhiversMC
                     .brlite_ack_i         (ack_tx[x][y]            ),
                     .brlite_flit_o        (flit_tx[x][y]           )
                 );
-
-                /* Hermes connection */
-                always_comb begin
-                    rx[x][y][HERMES_EAST]            = (x != N_PE_X - 1) ? tx[x + 1][y][HERMES_WEST]      : '0;
-                    credit_tx[x + 1][y][HERMES_WEST] = (x != N_PE_X - 1) ? credit_rx[x][y][HERMES_EAST]   : '0;
-                    data_rx[x][y][HERMES_EAST]       = (x != N_PE_X - 1) ? data_tx[x + 1][y][HERMES_WEST] : '0;
-
-                    rx[x][y][HERMES_WEST]            = (x != 0) ? tx[x - 1][y][HERMES_EAST]      : '0;
-                    credit_tx[x - 1][y][HERMES_EAST] = (x != 0) ? credit_rx[x][y][HERMES_WEST]   : '0;
-                    data_rx[x][y][HERMES_WEST]       = (x != 0) ? data_tx[x - 1][y][HERMES_EAST] : '0;
-
-                    rx[x][y][HERMES_NORTH]            = (y != N_PE_Y - 1) ? tx[x][y + 1][HERMES_SOUTH]      : '0;
-                    credit_tx[x][y + 1][HERMES_SOUTH] = (y != N_PE_Y - 1) ? credit_rx[x][y][HERMES_NORTH]   : '0;
-                    data_rx[x][y][HERMES_NORTH]       = (y != N_PE_Y - 1) ? data_tx[x][y + 1][HERMES_SOUTH] : '0;
-
-                    rx[x][y][HERMES_SOUTH]            = (y != 0) ? tx[x][y - 1][HERMES_NORTH]      : '0;
-                    credit_tx[x][y - 1][HERMES_NORTH] = (y != 0) ? credit_rx[x][y][HERMES_SOUTH]   : '0;
-                    data_rx[x][y][HERMES_SOUTH]       = (y != 0) ? data_tx[x][y - 1][HERMES_NORTH] : '0;
-
-                    if (address == ADDR_MA_INJ) begin
-                        rx[x][y][PORT_MA_INJ]      = ma_inj_tx;
-                        ma_inj_credit_tx           = credit_rx[x][y][PORT_MA_INJ];
-                        data_rx[x][y][PORT_MA_INJ] = ma_inj_data_tx;
-
-                        ma_inj_rx                    = tx[x][y][PORT_MA_INJ];
-                        credit_tx[x][y][PORT_MA_INJ] = ma_inj_credit_rx;
-                        ma_inj_data_rx               = data_tx[x][y][PORT_MA_INJ];
-                    end
-
-                    if (address == ADDR_APP_INJ) begin
-                        rx[x][y][PORT_APP_INJ]      = app_inj_tx && release_peripheral[x][y];
-                        app_inj_credit_tx           = credit_rx[x][y][PORT_APP_INJ];
-                        data_rx[x][y][PORT_APP_INJ] = app_inj_data_tx;
-
-                        app_inj_rx                    = tx[x][y][PORT_APP_INJ] && release_peripheral[x][y];
-                        credit_tx[x][y][PORT_APP_INJ] = app_inj_credit_rx;
-                        app_inj_data_rx               = data_tx[x][y][PORT_APP_INJ];
-                    end
-
-                    /* Insert the IO wiring for your component here if it connected to a port */
-                end
-
-                /* BrLite connection */
-                assign req_rx[x][y][BR_EAST]     = (x != N_PE_X - 1) ? req_tx[x + 1][y][BR_WEST]  : '0;
-                assign ack_tx[x + 1][y][BR_WEST] = (x != N_PE_X - 1) ? ack_rx[x][y][BR_EAST]      : '0;
-                assign flit_rx[x][y][BR_EAST]    = (x != N_PE_X - 1) ? flit_tx[x + 1][y][BR_WEST] : '0;
-
-                assign req_rx[x][y][BR_WEST]     = (x != 0) ? req_tx[x - 1][y][BR_EAST]  : '0;
-                assign ack_tx[x - 1][y][BR_EAST] = (x != 0) ? ack_rx[x][y][BR_WEST]      : '0;
-                assign flit_rx[x][y][BR_WEST]    = (x != 0) ? flit_tx[x - 1][y][BR_EAST] : '0;
-
-                assign req_rx[x][y][BR_NORTH]     = (y != N_PE_Y - 1) ? req_tx[x][y + 1][BR_SOUTH]  : '0;
-                assign ack_tx[x][y + 1][BR_SOUTH] = (y != N_PE_Y - 1) ? ack_rx[x][y][BR_NORTH]      : '0;
-                assign flit_rx[x][y][BR_NORTH]    = (y != N_PE_Y - 1) ? flit_tx[x][y + 1][BR_SOUTH] : '0;
-
-                assign req_rx[x][y][BR_SOUTH]     = (y != 0) ? req_tx[x][y - 1][BR_NORTH]  : '0;
-                assign ack_tx[x][y - 1][BR_NORTH] = (y != 0) ? ack_rx[x][y][BR_SOUTH]      : '0;
-                assign flit_rx[x][y][BR_SOUTH]    = (y != 0) ? flit_tx[x][y - 1][BR_NORTH] : '0;
             end
         end
     endgenerate
+
+////////////////////////////////////////////////////////////////////////////////
+// NoC connection (+ peripherals)
+////////////////////////////////////////////////////////////////////////////////
+
+    always_comb begin
+        localparam logic [7:0] MA_INJ_X = ADDR_MA_INJ[15:8];
+        localparam logic [7:0] MA_INJ_Y = ADDR_MA_INJ[7:0];
+
+        localparam logic [7:0] APP_INJ_X = ADDR_APP_INJ[15:8];
+        localparam logic [7:0] APP_INJ_Y = ADDR_APP_INJ[7:0];
+
+        for (int x = 0; x < N_PE_X; x++) begin
+            for (int y = 0; y < N_PE_Y; y++) begin
+                rx       [x][y][HERMES_EAST]  = (x != N_PE_X - 1) ? tx       [x + 1][y][HERMES_WEST]  : '0;
+                credit_tx[x][y][HERMES_EAST]  = (x != N_PE_X - 1) ? credit_rx[x + 1][y][HERMES_WEST]  : '0;
+                data_rx  [x][y][HERMES_EAST]  = (x != N_PE_X - 1) ? data_tx  [x + 1][y][HERMES_WEST]  : '0;
+
+                rx       [x][y][HERMES_WEST]  = (x != 0)          ? tx       [x - 1][y][HERMES_EAST]  : '0;
+                credit_tx[x][y][HERMES_WEST]  = (x != 0)          ? credit_rx[x - 1][y][HERMES_EAST]  : '0;
+                data_rx  [x][y][HERMES_WEST]  = (x != 0)          ? data_tx  [x - 1][y][HERMES_EAST]  : '0;
+
+                rx       [x][y][HERMES_NORTH] = (y != N_PE_Y - 1) ? tx       [x][y + 1][HERMES_SOUTH] : '0;
+                credit_tx[x][y][HERMES_NORTH] = (y != N_PE_Y - 1) ? credit_rx[x][y + 1][HERMES_SOUTH] : '0;
+                data_rx  [x][y][HERMES_NORTH] = (y != N_PE_Y - 1) ? data_tx  [x][y + 1][HERMES_SOUTH] : '0;
+
+                rx       [x][y][HERMES_SOUTH] = (y != 0)          ? tx       [x][y - 1][HERMES_NORTH] : '0;
+                credit_tx[x][y][HERMES_SOUTH] = (y != 0)          ? credit_rx[x][y - 1][HERMES_SOUTH] : '0;
+                data_rx  [x][y][HERMES_SOUTH] = (y != 0)          ? data_tx  [x][y - 1][HERMES_NORTH] : '0;
+            end
+        end
+        
+        rx       [MA_INJ_X][MA_INJ_Y][PORT_MA_INJ] = ma_inj_tx;
+        credit_tx[MA_INJ_X][MA_INJ_Y][PORT_MA_INJ] = ma_inj_credit_rx;
+        data_rx  [MA_INJ_X][MA_INJ_Y][PORT_MA_INJ] = ma_inj_data_tx;
+
+        rx       [APP_INJ_X][APP_INJ_Y][PORT_APP_INJ] = app_inj_tx && release_peripheral[APP_INJ_X][APP_INJ_Y];
+        credit_tx[APP_INJ_X][APP_INJ_Y][PORT_APP_INJ] = app_inj_credit_rx;
+        data_rx  [APP_INJ_X][APP_INJ_Y][PORT_APP_INJ] = app_inj_data_tx;
+
+        /* Insert the IO wiring for your component here if it connected to a port */
+        
+    end
+
+////////////////////////////////////////////////////////////////////////////////
+// BrLite connection
+////////////////////////////////////////////////////////////////////////////////
+
+    always_comb begin
+        for (int x = 0; x < N_PE_X; x++) begin
+            for (int y = 0; y < N_PE_Y; y++) begin
+                req_rx [x][y][BR_EAST]  = (x != N_PE_X - 1) ? req_tx [x + 1][y][BR_WEST]  : '0;
+                ack_tx [x][y][BR_EAST]  = (x != N_PE_X - 1) ? ack_rx [x + 1][y][BR_WEST]  : '0;
+                flit_rx[x][y][BR_EAST]  = (x != N_PE_X - 1) ? flit_tx[x + 1][y][BR_WEST]  : '0;
+
+                req_rx [x][y][BR_WEST]  = (x != 0)          ? req_tx [x - 1][y][BR_EAST]  : '0;
+                ack_tx [x][y][BR_WEST]  = (x != 0)          ? ack_rx [x - 1][y][BR_EAST]  : '0;
+                flit_rx[x][y][BR_WEST]  = (x != 0)          ? flit_tx[x - 1][y][BR_EAST]  : '0;
+
+                req_rx [x][y][BR_NORTH] = (y != N_PE_Y - 1) ? req_tx [x][y + 1][BR_SOUTH] : '0;
+                ack_tx [x][y][BR_NORTH] = (y != N_PE_Y - 1) ? ack_rx [x][y + 1][BR_SOUTH] : '0;
+                flit_rx[x][y][BR_NORTH] = (y != N_PE_Y - 1) ? flit_tx[x][y + 1][BR_SOUTH] : '0;
+
+                req_rx [x][y][BR_SOUTH] = (y != 0)          ? req_tx [x][y - 1][BR_NORTH] : '0;
+                ack_tx [x][y][BR_SOUTH] = (y != 0)          ? ack_rx [x][y - 1][BR_NORTH] : '0;
+                flit_rx[x][y][BR_SOUTH] = (y != 0)          ? flit_tx[x][y - 1][BR_NORTH] : '0;
+            end
+        end
+    end
+
 
 endmodule
