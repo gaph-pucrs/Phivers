16a1d4141c38f6ed03ceffff3571fa1bc553e7d6
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2024-04-23 17:32:56 -0300

    Fix sequential addressing

--- rtl/PhiversMC.sv
+++ rtl/PhiversMC.sv
@@ -168,7 +168,7 @@ module PhiversMC
         for (genvar x = 0; x < N_PE_X; x++) begin : gen_x
             for (genvar y = 0; y < N_PE_Y; y++) begin : gen_y
                 localparam logic [15:0] address  = {x[7:0], y[7:0]};
-                localparam logic [15:0] seq_addr = y * N_PE_X + x;
+                localparam logic [15:0] seq_addr = x * N_PE_Y + y;
 
                 PhiversPE #(
                     .ADDRESS      (address     ),
