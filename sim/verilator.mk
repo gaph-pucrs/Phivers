37b068d432ec46f8c53d1e8b852a1bcfa48e2990
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2024-05-07 18:06:54 -0300

    Re-unify testbenches

--- sim/verilator.mk
+++ sim/verilator.mk
@@ -3,39 +3,24 @@ VERILATOR = verilator
 OBJ_DIR = obj_dir
 OBJ = $(patsubst %.cpp, $(OBJ_DIR)/%.o, $(CPPTOP))
 
-OPT      = -fdata-sections -ffunction-sections -flto
-OPT_FAST = -O3
-SVOPT    = --x-initial fast --noassert
+SVOPT    = --x-initial fast --noassert -O3
+CFLAGS   = -ffunction-sections -fdata-sections -flto
+LDFLAGS  = -Wl,--gc-sections -flto
 
 ifeq ($(TRACE), 1)
 	TRACE_VERILATOR = --trace-fst
 	DEF_TRACE = -DTRACE=1
-	LD_TRACE = -lz
 endif
 
-CPPFLAGS = $(OPT) $(OPT_FAST) `pkgconf --cflags verilator` $(DEF_TRACE)
-LDFLAGS = -lpthread -Wl,--gc-sections,-flto $(LD_TRACE)
-VERILATE_FLAGS = -Wall --quiet --autoflush --cc --timescale 1ns/1ns $(SVOPT) $(TRACE_VERILATOR)
+VERILATE_FLAGS = -Wall --quiet --autoflush --timescale 1ns/1ns -CFLAGS "$(CFLAGS)" -LDFLAGS "$(LDFLAGS)" $(SVOPT) $(TRACE_VERILATOR)
 
 verilator:
 	@mkdir -p debug
 	@./$(TARGET)
 
-$(TARGET): $(OBJ) $(OBJ_DIR)/V$(TOP)__ALL.a
-	@printf "${COR}Linking %s ... ${NC}\n" "$@"
-	@g++ $(OBJ) $(OBJ_DIR)/*.a -o $@ $(LDFLAGS)
-
-$(OBJ_DIR)/%.o: %.cpp $(OBJ_DIR)/V$(TOP).cpp
-	@printf "${COR}Compiling %s ... ${NC}\n" "$<"
-	@g++ -c $< -o $@ -I$(OBJ_DIR) $(CPPFLAGS)
-
-$(OBJ_DIR)/V$(TOP).cpp: $(SVSRC)
-	@printf "${COR}Verilating %s ... ${NC}\n" "$(TOP)"
-	@$(VERILATOR) --top $(TOP) $(SVSRC) $(SVFLAGS) $(VERILATE_FLAGS)
-
-$(OBJ_DIR)/V$(TOP)__ALL.a: $(OBJ_DIR)/V$(TOP).cpp
-	@printf "${COR}Compiling %s ... ${NC}\n" "$@"
-	@+make AR=gcc-ar OPT_FAST="$(OPT_FAST)" OPT="$(OPT)" -C $(OBJ_DIR) -f V$(TOP).mk -s
+$(TARGET): $(SVSRC)
+	@printf "${COR}Building %s ... ${NC}\n" "$@"
+	@+verilator --binary $(SVSRC) --top $(TOP) $(SVFLAGS) $(VERILATE_FLAGS) -MAKEFLAGS -s -o ../$@
 
 clean-verilator:
 	@rm -rf $(OBJ_DIR)
