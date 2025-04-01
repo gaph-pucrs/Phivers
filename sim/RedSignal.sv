fa5364f5738209786d4b4c25bbf8d659735ed723
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2025-04-01 11:38:37 -0300

    Add edge filter to RS

--- sim/RedSignal.sv
+++ sim/RedSignal.sv
@@ -21,6 +21,9 @@ module RedSignal
     int unsigned cycles_min;
     int unsigned cycles_max;
     int unsigned chance;
+    int filter_app;
+    int filter_prod;
+    int filter_cons;
 
     int cfg;
     int log;
@@ -38,10 +41,13 @@ module RedSignal
         end
         else begin
             enabled = 1;
-            $fscanf(cfg, "%d", tick_begin);
-            $fscanf(cfg, "%d", cycles_min);
-            $fscanf(cfg, "%d", cycles_max);
-            $fscanf(cfg, "%d", chance    );
+            $fscanf(cfg, "%d", tick_begin );
+            $fscanf(cfg, "%d", cycles_min );
+            $fscanf(cfg, "%d", cycles_max );
+            $fscanf(cfg, "%d", chance     );
+            $fscanf(cfg, "%d", filter_app );
+            $fscanf(cfg, "%d", filter_prod);
+            $fscanf(cfg, "%d", filter_cons);
             $fclose(cfg);
 
             log = $fopen($sformatf("debug/link/rs%0dx%0d-%s.log", ADDRESS[15:8], ADDRESS[7:0], PORT), "w");
@@ -102,44 +108,6 @@ module RedSignal
         end
     end
 
-    always_comb begin
-        case (state)
-            HEADER:  begin
-                if (received)
-                    next_state = ($time() >= 64'(tick_begin)) ? SIZE : EOP;
-                else
-                    next_state = HEADER;
-            end
-            SIZE:    next_state = received ? SERVICE : SIZE;
-            SERVICE: begin
-                if (received) begin
-                    if (data_tx_i == 32'h00000001)
-                        next_state = (next_hang < chance) ? PROD : NEXT;
-                    else
-                        next_state = EOP;
-                end
-                else begin
-                    next_state = SERVICE;
-                end
-            end
-            PROD:      next_state = received               ? CONS      : PROD;
-            CONS:      next_state = received               ? SRCPE     : CONS;
-            SRCPE:     next_state = received               ? TIMESTAMP : SRCPE;
-            TIMESTAMP: next_state = received               ? LOG       : HANG;
-            LOG:       next_state = HANG;
-            HANG:      next_state = (hang_cycles == 0)     ? NEXT      : HANG;
-            NEXT:      next_state = EOP;
-            EOP:       next_state = (received && eop_tx_i) ? HEADER    : EOP;
-        endcase
-    end
-
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-        if (!rst_ni)
-            state <= HEADER;
-        else if (enabled)
-            state <= next_state;
-    end
-
     logic [31:0] producer;
     always_ff @(posedge clk_i or negedge rst_ni) begin
         if (!rst_ni)
@@ -154,6 +122,60 @@ module RedSignal
             consumer <= '0;
         else if (state == CONS)
             consumer <= data_tx_i;
+    end
+
+    always_comb begin
+        case (state)
+            HEADER:    begin
+                if (received)
+                    next_state = ($time() >= 64'(tick_begin)) ? SIZE : EOP;
+                else
+                    next_state = HEADER;
+            end
+            SIZE:    next_state = received ? SERVICE : SIZE;
+            SERVICE:   begin
+                if (received)
+                    next_state = (data_tx_i == 32'h00000001) ? PROD : EOP;
+                else
+                    next_state = SERVICE;
+            end
+            PROD:      next_state = received ? CONS : PROD;
+            CONS:      begin
+                if (
+                       (filter_app  != -1 && producer[15:8] != filter_app [7:0])
+                    || (filter_prod != -1 && producer[ 7:0] != filter_prod[7:0])
+                )
+                    next_state = EOP;
+                else
+                    next_state = received ? SRCPE : CONS;
+            end
+            SRCPE:     begin
+                if (
+                       (filter_app  != -1 && consumer[15:8] != filter_app [7:0])
+                    || (filter_cons != -1 && consumer[ 7:0] != filter_cons[7:0])
+                )
+                    next_state = EOP;
+                else
+                    next_state = received ? TIMESTAMP : SRCPE;
+            end
+            TIMESTAMP: begin
+                if (next_hang >= chance)
+                    next_state = NEXT;
+                else
+                    next_state = received ? LOG : TIMESTAMP;
+            end
+            LOG:       next_state = HANG;
+            HANG:      next_state = (hang_cycles == 0)     ? NEXT      : HANG;
+            NEXT:      next_state = EOP;
+            EOP:       next_state = (received && eop_tx_i) ? HEADER    : EOP;
+        endcase
+    end
+
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni)
+            state <= HEADER;
+        else if (enabled)
+            state <= next_state;
     end
 
     logic [31:0] timestamp;
