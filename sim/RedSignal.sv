65c2efb8569a1e0e9509407b4cbd1a4ceac0ce3c
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2025-04-01 10:42:37 -0300

    Add RS logging

--- sim/RedSignal.sv
+++ sim/RedSignal.sv
@@ -23,6 +23,7 @@ module RedSignal
     int unsigned chance;
 
     int cfg;
+    int log;
     initial begin
         cfg = $fopen($sformatf("link/rs%0dx%0d-%s.cfg", ADDRESS[15:8], ADDRESS[7:0], PORT), "r");
         if (cfg == '0) begin
@@ -43,6 +44,9 @@ module RedSignal
             $fscanf(cfg, "%d", chance    );
             $fclose(cfg);
 
+            log = $fopen($sformatf("debug/link/rs%0dx%0d-%s.log", ADDRESS[15:8], ADDRESS[7:0], PORT), "w");
+            $fwrite(log, "timestamp,prod,cons,cycles\n");
+
             $display(
                 "[%7.3f] [RS %02dx%02d-%s] Will hang for %0d to %0d cycles with %0d chance", 
                 $time()/1_000_000.0, 
@@ -60,6 +64,11 @@ module RedSignal
         HEADER,
         SIZE,
         SERVICE,
+        PROD,
+        CONS,
+        SRCPE,
+        TIMESTAMP,
+        LOG, 
         HANG,
         NEXT,
         EOP
@@ -75,12 +84,10 @@ module RedSignal
     int unsigned hang_cycles;
 
     always_ff @(posedge clk_i or negedge rst_ni) begin
-        if (!rst_ni) begin
+        if (!rst_ni)
             next_hang <= $urandom_range(0, 99);
-        end
-        else if (state == NEXT) begin
+        else if (state == NEXT)
             next_hang <= $urandom_range(0, 99);
-        end
     end
 
     always_ff @(posedge clk_i or negedge rst_ni) begin
@@ -106,33 +113,23 @@ module RedSignal
             SIZE:    next_state = received ? SERVICE : SIZE;
             SERVICE: begin
                 if (received) begin
-                    if (data_tx_i == 32'h00000001) begin
-                        if (next_hang < chance) begin
-                            next_state = HANG;
-                            $display(
-                                "[%7.3f] [RS %02dx%02d-%s] Entering malicious state for %0d cycles", 
-                                $time()/1_000_000.0, 
-                                ADDRESS[15:8], 
-                                ADDRESS[7:0], 
-                                PORT, 
-                                hang_cycles
-                            );
-                        end
-                        else begin
-                            next_state = NEXT;
-                        end
-                    end
-                    else begin
+                    if (data_tx_i == 32'h00000001)
+                        next_state = (next_hang < chance) ? PROD : NEXT;
+                    else
                         next_state = EOP;
-                    end
                 end
                 else begin
                     next_state = SERVICE;
                 end
             end
-            HANG: next_state = (hang_cycles == 0)     ? NEXT   : HANG;
-            NEXT: next_state = EOP;
-            EOP:  next_state = (received && eop_tx_i) ? HEADER : EOP;
+            PROD:      next_state = received               ? CONS      : PROD;
+            CONS:      next_state = received               ? SRCPE     : CONS;
+            SRCPE:     next_state = received               ? TIMESTAMP : SRCPE;
+            TIMESTAMP: next_state = received               ? LOG       : HANG;
+            LOG:       next_state = HANG;
+            HANG:      next_state = (hang_cycles == 0)     ? NEXT      : HANG;
+            NEXT:      next_state = EOP;
+            EOP:       next_state = (received && eop_tx_i) ? HEADER    : EOP;
         endcase
     end
 
@@ -141,6 +138,46 @@ module RedSignal
             state <= HEADER;
         else if (enabled)
             state <= next_state;
+    end
+
+    logic [31:0] producer;
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni)
+            producer <= '0;
+        else if (state == PROD)
+            producer <= data_tx_i;
+    end
+
+    logic [31:0] consumer;
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni)
+            consumer <= '0;
+        else if (state == CONS)
+            consumer <= data_tx_i;
+    end
+
+    logic [31:0] timestamp;
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni)
+            timestamp <= '0;
+        else if (state == TIMESTAMP)
+            timestamp <= data_tx_i;
+    end
+
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni) begin
+            ;
+        end
+        else if (state == LOG) begin
+            $fwrite(
+                log, 
+                "%0d,%0d,%0d,%0d\n",
+                timestamp, 
+                producer, 
+                consumer, 
+                hang_cycles
+            );
+        end
     end
 
     always_comb begin
@@ -152,6 +189,10 @@ module RedSignal
             rx_o    = '0;
             cr_tx_o = '0;
         end
+    end
+
+    final begin
+        $fclose(log);
     end
 
 endmodule
